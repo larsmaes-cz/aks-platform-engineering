eef889bf01a727256af8e6de7bf48ffe949d4418
Lars Maes <lars.maes@cz.nl>
2026-06-23 09:49:19 +0200

    Add GitHub catalog module and update app configuration

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -73,4 +73,7 @@ backend.add(import('@backstage-community/plugin-argocd-backend'));
 // roadie scaffolder utils
 backend.add(import('@roadiehq/scaffolder-backend-module-utils'));
 
+//Github Catalog
+backend.add(import('@backstage/plugin-catalog-backend-module-github'));
+
 backend.start();
